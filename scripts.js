b94e0a1a7dd16cbd3786fcafb7401abb73603897
VictorHugoCid <victorcidpereira@gmail.com>
2022-06-14 13:02:29 -0300

    LayoutPronto + testes 14-06-2022 as 13h00mim

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function choose(element){
+    
+    const carta = element.querySelector(".imagem");
+    /* carta.classList.toggle("hidden-gif"); */
+    carta.src="imagens/gif/bobrossparrot.gif"
+
+    /* carta.innerHTML = `<img src="imagens/gif/bobrossparrot.gif" class="gif"></img>`
+    console.log(carta); */
+    
+
+    /* card.innerHTML */
+
+    /* cardImage = element.querySelector(".card img")
+    console.log(card) 
+    cardImage.classList.toggle("hidden-img") */
+
+    /* gif = element.querySelector(".card .gif")
+    console.log(gif) 
+    gif.classList.toggle("hidden-gif") */
+
+
+}
+
+function randomImg(){
+    
+}
